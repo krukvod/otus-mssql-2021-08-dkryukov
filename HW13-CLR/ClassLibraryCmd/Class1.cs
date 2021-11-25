9685b628ef57031669eba3f8282d591941e2da78
DKryukov <krukovd@mail.ru>
2021-11-25 21:20:09 +0300

    dop classlibrarycmd

---
+++ HW13-CLR/ClassLibraryCmd/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Diagnostics;
+
+namespace ClassLibraryCmd
+{
+    public class Class1
+    {
+        //Тег, определяющий, что данная функция возвращает таблицу
+        [Microsoft.SqlServer.Server.SqlFunction(FillRowMethodName = "FillRow",
+        TableDefinition = "txt nvarchar(4000)")]
+        public static System.Collections.IEnumerable FCmd(SqlString commandToRun, SqlString args)
+        {
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = commandToRun.Value;
+            p.StartInfo.Arguments = args.Value;
+            p.Start();
+
+            string value = p.StandardOutput.ReadToEnd();
+            string[] spl = value.Split('\n');
+
+            p.WaitForExit();
+            return spl.ToArray();
+        }
+
+        //Функция заполнения таблицы
+        public static void FillRow(Object obj, out string stringElement)
+        {
+            stringElement = obj.ToString();//Возвращает в таблицу строку
+        }
+    }
+}
